51ca9e50585e5375cdd41eab2c0ab7b7a6d26afd
연 <mogoo22@naver.com>
2023-07-30 07:24:04 +0900

    ✨ ChartMaker 컴포넌트 생성 - chartOpen 상태에 따라 점수를 입력받는 ScoreBoard 컴포넌트 또는 결과 차트를 보여주는 Chart 컴포넌트를 렌더링한다. - scores 상태로 점수를 관리한다.

---
+++ 1-producing-chart/my-app/src/App.js
@@ -0,0 +1,11 @@
+import ChartMaker from "./ChartMaker";
+
+function App() {
+  return (
+    <div className="App">
+      <ChartMaker />
+    </div>
+  );
+}
+
+export default App;
---
+++ 1-producing-chart/my-app/src/ChartMaker.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import styled from "styled-components";
+import Chart from "./Chart";
+import { STUDENTS } from "./constants";
+import ScoreBoard from "./ScoreBoard";
+
+const StyledChart = styled.div`
+  min-height: 100vh;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  gap: 20px;
+  text-align: center;
+  font-family: monospace;
+
+  button {
+    border: 1px solid black;
+    border-radius: 8px;
+    padding: 10px;
+    cursor: pointer;
+    background: inherit;
+    &:hover {
+      background: lightGray;
+    }
+  }
+`;
+
+export const Box = styled.div`
+  border: 1px solid black;
+  border-radius: 8px;
+  padding: 20px;
+`;
+
+const Head = styled(Box)`
+  font-weight: 600;
+  font-size: 25px;
+`;
+
+const Body = styled(Box)`
+  width: 300px;
+  height: 450px;
+  padding: 35px 20px;
+  font-size: 15px;
+`;
+
+export interface ChartDataType {
+  [name: string]: number;
+}
+
+const ChartMaker = () => {
+  const initScores = STUDENTS.reduce((prev, cur) => ({ ...prev, [cur]: 0 }), {});
+  const [scores, setScores] = useState<ChartDataType>(initScores);
+  const [chartOpen, setChartOpen] = useState<boolean>(false);
+
+  const resetScores = () => {
+    setScores(initScores);
+  };
+
+  return (
+    <StyledChart>
+      <Head>Games Score Chart</Head>
+      <Body>
+        {chartOpen ? (
+          <Chart data={scores} setChartOpen={setChartOpen} />
+        ) : (
+          <ScoreBoard scores={scores} resetScores={resetScores} setScores={setScores} setChartOpen={setChartOpen} />
+        )}
+      </Body>
+    </StyledChart>
+  );
+};
+
+export default ChartMaker;
