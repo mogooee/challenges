4904fc08c7012d8417cc76a60ec15b0e9df9f734
연 <mogoo22@naver.com>
2023-07-30 07:28:05 +0900

    ✨ ScoreBoard 컴포넌트 생성 - 입력받지 못한 점수가 있는 경우 Make Chart 버튼이 비활성화된다. - Reset Data 버튼을 누르면 입력받은 모든 점수가 초기화된다.

--- 1-producing-chart/my-app/src/ScoreBoard.tsx
+++ 1-producing-chart/my-app/src/ScoreBoard.tsx
@@ -1,4 +1,41 @@
+import styled from "styled-components";
 import { SCORE } from "./constants";
+import { ChartDataType } from "./ChartMaker";
+
+const ScoreTable = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2.5fr;
+  gap: 20px;
+
+  label,
+  input {
+    border: 1px solid black;
+    border-radius: 8px;
+    padding: 10px;
+  }
+
+  input {
+    outline: none;
+    text-align: center;
+  }
+`;
+
+const ControlBox = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 60px;
+
+  button {
+    border: 1px solid black;
+    border-radius: 8px;
+    padding: 15px;
+    cursor: pointer;
+    background: inherit;
+    &:hover {
+      background: lightGray;
+    }
+  }
+`;
 
 interface InputType {
   name: string;
@@ -27,3 +64,38 @@ const Input = ({ name, score, setScores }: InputType) => {
     </>
   );
 };
+
+interface ScoreBoardType {
+  scores: ChartDataType;
+  setScores: React.Dispatch<React.SetStateAction<ChartDataType>>;
+  resetScores: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  setChartOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ScoreBoard = ({ scores, resetScores, setScores, setChartOpen }: ScoreBoardType) => {
+  const hasBlankScore: boolean = Object.values(scores).filter((score) => score === 0).length > 0;
+
+  const openChart = () => {
+    setChartOpen(true);
+  };
+
+  return (
+    <div>
+      <ScoreTable>
+        <span>Student</span>
+        <span>Score</span>
+        {Object.keys(scores).map((student) => (
+          <Input key={student} name={student} score={scores[student]} setScores={setScores} />
+        ))}
+      </ScoreTable>
+      <ControlBox>
+        <button disabled={hasBlankScore} onClick={openChart}>
+          Make Chart
+        </button>
+        <button onClick={resetScores}>Reset Data</button>
+      </ControlBox>
+    </div>
+  );
+};
+
+export default ScoreBoard;
