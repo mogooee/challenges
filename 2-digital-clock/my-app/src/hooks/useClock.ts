8abf0a715b1392087298032c655b98158bd7bb4e
연 <mogoo22@naver.com>
2023-08-03 15:53:40 +0900

    ✨ Clock Radio 버튼으로 12시간 모드와 24시간 모드를 선택할 수 있다.

--- 2-digital-clock/my-app/src/hooks/useClock.ts
+++ 2-digital-clock/my-app/src/hooks/useClock.ts
@@ -8,23 +8,34 @@ interface Clock {
   sec: number;
 }
 
+type ClockMode = '12-HOUR' | '24-HOUR';
+
 const useClock = () => {
+  const [mode, setMode] = useState<ClockMode>('12-HOUR');
   const [clock, setClock] = useState<Clock>({
     hour: new Date().getHours(),
     minute: new Date().getMinutes(),
     sec: new Date().getSeconds(),
   });
 
+  const clickRadioButtonHandler = () => {
+    if (mode === '12-HOUR') {
+      setMode('24-HOUR');
+      return;
+    }
+    setMode('12-HOUR');
+  };
+
   const clockRadio: RadioData[] = [
     {
       title: '12',
       checked: true,
-      onClick: () => {},
+      onClick: clickRadioButtonHandler,
     },
     {
       title: '24',
       checked: false,
-      onClick: () => {},
+      onClick: clickRadioButtonHandler,
     },
   ];
 
@@ -33,8 +44,9 @@ const useClock = () => {
     setClock({ hour: date.getHours(), minute: date.getMinutes(), sec: date.getSeconds() });
   };
 
-  const hour1 = getTens(clock.hour);
-  const hour2 = getUnits(clock.hour);
+  const meridiem = clock.hour < 12 ? 'AM' : 'PM';
+  const hour1 = getTens(mode === '12-HOUR' && clock.hour > 12 ? clock.hour - 12 : clock.hour);
+  const hour2 = getUnits(mode === '12-HOUR' && clock.hour > 12 ? clock.hour - 12 : clock.hour);
   const minute1 = getTens(clock.minute);
   const minute2 = getUnits(clock.minute);
   const flicker = clock.sec % 2;
