29115b711aa15a7094e15dd49963c41891e95a90
연 <mogoo22@naver.com>
2023-08-07 00:50:08 +0900

    ✨ 별위에 마우스를 올리면 별의 절반 또는 전체에 색상이 변한다. - offsetX가 현재 이벤트가 발생한 currentTarget의 넓이보다 절반 이하이면 절반의 색상이 변하고 절반 이상이면 전체 색상이 변한다.

--- 3-star-rating/src/js/components/Star.js
+++ 3-star-rating/src/js/components/Star.js
@@ -4,6 +4,7 @@ export class Star {
   constructor(value) {
     this.value = value;
     this.init();
+    this.setEvents();
   }
 
   init = () => {
@@ -16,7 +17,42 @@ export class Star {
   };
 
   createHTML = () => {
-    return `<button class='star'></button>`;
+    const state =
+      this.value === RATING.INIT.value ? RATING.INIT.class : RATING.HALF.value ? RATING.HALF.class : RATING.ON.class;
+    return `<button class='star ${state}'></button>`;
+  };
+
+  setEvents = () => {
+    this.setTarget();
+    this.target?.addEventListener('mousemove', this.mouseMoveStar);
+  };
+
+  setTarget = () => {
+    const star = $('.star');
+    this.target = star;
+  };
+
+  noHighlightStar = (starElemnt) => {
+    starElemnt.classList.remove('half', 'on');
+  };
+
+  highlightHalfStar = (starElement) => {
+    starElement.classList.add('half');
+    starElement.classList.remove('on');
+  };
+
+  highlightStar = (starElement) => {
+    starElement.classList.add('on');
+    starElement.classList.remove('half');
+  };
+
+  mouseMoveStar = (event) => {
+    const star = $('.star');
+        if (event.offsetX < event.currentTarget.offsetWidth / 2) {
+          this.highlightHalfStar(stars[i]);
+        } else {
+          this.highlightStar(stars[i]);
+        }
   };
 
 export default Star;
