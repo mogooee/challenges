60f33e9500e5370c69232e7e39c1c234d912b488
연 <mogoo22@naver.com>
2023-08-14 23:59:45 +0900

    ✨<Notification> 자동 타이머에 대한 진행률을 표시한다.

--- 5-toast-notification/src/components/ToastNotification/Notification.ts
+++ 5-toast-notification/src/components/ToastNotification/Notification.ts
@@ -35,6 +35,7 @@ class Notification {
               ${icon[this.type]}
               <span>${this.message}</span>
               <button class='cancel-btn'>${icon.cancel}</button>
+              <progress value="1" max="1"></progress>
            </div>`;
   };
 
@@ -50,12 +51,27 @@ class Notification {
   };
 
   setAutoTimer = () => {
+    const { progressDOM, progressTimer } = this.setProgressBar();
     setTimeout(() => {
+      progressDOM.value = 0;
+      clearInterval(progressTimer);
       this.target?.classList.toggle('on');
     }, this.autoTimer);
     setTimeout(() => {
       this.target?.remove();
     }, this.autoTimer + ANIMATION_DELAY);
+  };
+
+  setProgressBar = () => {
+    let progress = 0;
+    const [progressTime, progressMax] = [100, 1];
+    const progressDOM = this.target?.querySelector('progress')!;
+    const progressTimer = setInterval(() => {
+      const progressRate = progress / (this.autoTimer / progressTime);
+      progressDOM.value = progressMax - progressRate;
+      progress += 1;
+    }, progressTime);
+    return { progressDOM, progressTimer };
   };
 }
 
