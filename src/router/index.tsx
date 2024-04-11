829e3dbd6c3c250f010550fc069d85e45107500a
연 <mogoo22@naver.com>
2024-04-11 19:00:02 +0900

    refactor: sidebar router in outlet

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { Navigate, BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Navigate, BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
 import ProducingChart from '../pages/ProducingChart';
 import DigitalClock from '../pages/DigitalClock';
 import StarRating from '../pages/StarRating';
@@ -8,20 +8,30 @@ import Calculator from '../pages/Calculator';
 import PostIt from '../pages/PostIt';
 import SideBar from '../components/SideBar';
 
+const Dashboard = () => {
+  return (
+    <>
+      <SideBar />
+      <Outlet />
+    </>
+  );
+};
+
 const Routers = (): React.ReactElement => {
   return (
-    <BrowserRouter>
-      <SideBar />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<ProducingChart />} />
-        <Route path="/producing-chart" index element={<ProducingChart />} />
-        <Route path="/digital-clock" element={<DigitalClock />} />
-        <Route path="/star-rating" element={<StarRating />} />
-        {/* <Route path="/presentation" element={<Presentation />} /> */}
-        <Route path="/toast-notification" element={<Notification />} />
-        <Route path="/calculator" element={<Calculator />} />
-        <Route path="/post-it" element={<PostIt />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<ProducingChart />} />
+          <Route path="producing-chart" element={<ProducingChart />} />
+          <Route path="digital-clock" element={<DigitalClock />} />
+          <Route path="star-rating" element={<StarRating />} />
+          {/* <Route path="presentation" element={<Presentation />} /> */}
+          <Route path="toast-notification" element={<Notification />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="post-it" element={<PostIt />} />
+          {/* <Route path="*" element={<Navigate to="/" />} /> */}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
